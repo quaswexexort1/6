121a641fb0b5e5baf9173bec61e2dec9524ed3a2
SHEL <SHEL@DESKTOP-6SFOS1M>
2024-10-24 22:13:19 +0200

    Добавьте файлы проекта.

---
+++ Оператор выбора/Program.cs
@@ -0,0 +1,18 @@
+﻿try
+{
+    int a = 10;
+    int b = 22;
+    int c = 41;
+    int min = Math.Min(Math.Min(a, c), b);
+    Console.WriteLine($"Наименьшим числом является {min}");
+    a = 76; b = 5; c = 37;
+    min = Math.Min(Math.Min(a, c), b);
+    Console.WriteLine($"Наименьшим числом является {min}");
+    a = 11; b = 75; c = 14;
+    min = Math.Min(Math.Min(a, c), b);
+    Console.WriteLine($"Наименьшим числом является {min}");
+}
+catch
+{
+    Console.WriteLine("Error!");
+}
